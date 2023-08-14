a6b931670c1a8a6671b535d8143d1a91026ba93a
Goncharenko Nikita <goncharenkonikitae@gmail.com>
2023-08-14 20:31:37 +0300

    feat: add axios integration

--- src/pages/Securedpage.js
+++ src/pages/Securedpage.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import axios from "axios";
+import React from "react";
+import {useKeycloak} from "@react-keycloak/web";
 
+const baseURL = "http://127.0.0.1:8080/local/me";
 
 const Secured = () => {
+    const {keycloak, initialized} = useKeycloak();
+    const [me, setMe] = React.useState(null);
+
+    axios.defaults.headers.common['Authorization'] = `Bearer ${keycloak.token}`;
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setMe(response.data);
+        });
+    }, []);
 
     return (
         <div>
-            <h1 className="text-black text-4xl">Welcome to the Protected Page.</h1>
+            <p>{JSON.stringify(me)}</p>
         </div>
     );
 };
